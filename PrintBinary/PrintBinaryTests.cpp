3d85ee4eb0b33c35291bc683cdb8501379ef56ef
Daniil Tretyakov <tretyakov.daniil.education@gmail.com>
2023-03-26 01:37:01 +0300

    added some extra tests

--- PrintBinary/PrintBinaryTests.cpp
+++ PrintBinary/PrintBinaryTests.cpp
@@ -1,6 +1,78 @@
 #include "PrintBinary.hpp"
 #include <gtest/gtest.h>
 
+TEST(PrintBinaryTests, PositiveTwo)
+{
+    testing::internal::CaptureStdout();
+    PrintBinary(2);
+    std::string actual = testing::internal::GetCapturedStdout();
+    std::string expected("00000000000000000000000000000010\n");
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(PrintBinaryTests, NegativeTwo)
+{
+    testing::internal::CaptureStdout();
+    PrintBinary(-2);
+    std::string actual = testing::internal::GetCapturedStdout();
+    std::string expected("11111111111111111111111111111110\n");
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(PrintBinaryTests, PositiveFour)
+{
+    testing::internal::CaptureStdout();
+    PrintBinary(4);
+    std::string actual = testing::internal::GetCapturedStdout();
+    std::string expected("00000000000000000000000000000100\n");
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(PrintBinaryTests, NegativeFour)
+{
+    testing::internal::CaptureStdout();
+    PrintBinary(-4);
+    std::string actual = testing::internal::GetCapturedStdout();
+    std::string expected("11111111111111111111111111111100\n");
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(PrintBinaryTests, PositiveEight)
+{
+    testing::internal::CaptureStdout();
+    PrintBinary(8);
+    std::string actual = testing::internal::GetCapturedStdout();
+    std::string expected("00000000000000000000000000001000\n");
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(PrintBinaryTests, NegativeEight)
+{
+    testing::internal::CaptureStdout();
+    PrintBinary(-8);
+    std::string actual = testing::internal::GetCapturedStdout();
+    std::string expected("11111111111111111111111111111000\n");
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(PrintBinaryTests, PositiveSixteen)
+{
+    testing::internal::CaptureStdout();
+    PrintBinary(16);
+    std::string actual = testing::internal::GetCapturedStdout();
+    std::string expected("00000000000000000000000000010000\n");
+    EXPECT_EQ(actual, expected);
+}
+
+TEST(PrintBinaryTests, NegativeSixteen)
+{
+    testing::internal::CaptureStdout();
+    PrintBinary(-16);
+    std::string actual = testing::internal::GetCapturedStdout();
+    std::string expected("11111111111111111111111111110000\n");
+    EXPECT_EQ(actual, expected);
+}
+
 TEST(PrintBinaryTests, PositiveTen)
 {
     testing::internal::CaptureStdout();
